6dc7684cdc7a983f7c9d362dbde1b7a324a6f8a4
Evgeny Shulgin <izaronplatz@gmail.com>
2023-01-04 04:07:10 +0400

    Load compile database from source file

--- src/lib/driver/driver.cpp
+++ src/lib/driver/driver.cpp
@@ -7,31 +7,28 @@
 
 using namespace Waffle;
 
-//int main(int argc, const char** argv) {
-    //std::string errorMsg;
-    //auto db = clang::tooling::CompilationDatabase::loadFromDirectory(".", errorMsg);
-    //if (!db) {
-        //llvm::errs() << "database not loaded, error:\n" << errorMsg;
-        //return 1;
-    //}
-
-    //std::cout << "Hello world" << std::endl;
-    //for (const auto& file : db->getAllFiles()) {
-        //std::cout << file << std::endl;
-    //}
-//}
-
 int main(int argc, const char** argv) {
     for (const auto& module : ModuleRegistry::GetModules()) {
         llvm::errs() << "module " << module->Name() << " is available\n";
     }
 
-    if (argc < 2) {
-        llvm::errs() << "please provide path!\n";
+    if (argc < 3) {
+        llvm::errs() << "please provide path to source file and output directory\n";
         return 1;
     }
 
-    FileManager fileMgr{argv[1]};
+    std::string errorMsg;
+    auto db = clang::tooling::CompilationDatabase::autoDetectFromSource(argv[1], errorMsg);
+    if (!db) {
+        llvm::errs() << "database not loaded, error:\n" << errorMsg;
+        return 1;
+    }
+
+    for (const auto& file : db->getAllFiles()) {
+        std::cout << file << std::endl;
+    }
+
+    FileManager fileMgr{argv[2]};
     Context ctx{
         .FileMgr = fileMgr,
     };
