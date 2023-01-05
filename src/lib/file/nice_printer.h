1662447772962818febec4ea52db40d154278fce
Evgeny Shulgin <izaronplatz@gmail.com>
2023-01-05 18:36:42 +0400

    Add NicePrinter

---
+++ src/lib/file/nice_printer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "file.h"
+
+namespace Waffle {
+
+class NicePrinter {
+public:
+    explicit NicePrinter(IFilePrinter& printer);
+
+    void AddPreabmle(std::string_view source);
+    void Include(std::string_view file);
+    void NewLine(int count = 1);
+    void OpenNamespace();
+    void CloseNamespace();
+    void AddIndent(int diff = 4);
+    void DecreaseIndent(int diff = 4);
+
+    NicePrinter& operator<<(std::string_view s);
+
+private:
+    int indent_ = 0;
+    bool addIndent_ = true;
+    IFilePrinter& proxyPrinter_;
+};
+
+} // namespace Waffle
