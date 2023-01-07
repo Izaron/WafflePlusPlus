f7083ac2120cfcd1e704d622a06ecba48fd2a7a7
Evgeny Shulgin <izaronplatz@gmail.com>
2023-01-07 23:50:44 +0400

    Rename module.cpp to register.cpp

--- src/modules/json_struct/register.cpp
+++ src/modules/json_struct/register.cpp
@@ -1,9 +1,19 @@
 #include "collector.h"
 #include "common.h"
-#include "module.h"
 #include "printer.h"
 
 #include <lib/registry/registry.h>
+
+namespace Waffle {
+
+class JsonStructModule : public IModule {
+public:
+    std::string_view Name() const override;
+    std::span<const std::string_view> Commands() const override;
+    void Do(Context& context) const override;
+};
+
+} // namespace Waffle
 
 using namespace Waffle;
 
