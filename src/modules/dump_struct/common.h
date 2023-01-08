c44784b4f3aea0719ab2c995f1367e6736721565
Evgeny Shulgin <izaronplatz@gmail.com>
2023-01-08 21:27:44 +0400

    Remove useless dump_struct module

--- src/modules/dump_struct/common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <clang/AST/Decl.h>
-
-namespace Waffle::DumpStruct {
-
-using StructDatas = std::vector<const clang::RecordDecl*>;
-
-} // namespace Waffle::DumpStruct
--- src/modules/json_struct/printer.cpp
+++ src/modules/json_struct/printer.cpp
@@ -1,5 +1,7 @@
 #include "printer.h"
 
+#include <inja/inja.hpp>
+
 #include <lib/file/file.h>
 #include <lib/string_util/string_util.h>
 
@@ -21,11 +23,32 @@ public:
         const std::string_view inFile = StringUtil::AfterLastSlash(Ctx_.InFile);
         const std::string outputFile = StringUtil::InsertBeforeExt(inFile, "json_struct");
         auto& printer = Ctx_.FileManager.GetOrCreateFilePrinter(outputFile);
+
+        inja::json dataJson;
+        dataJson["source_file"] = StringUtil::RemoveLastExt(inFile);
+        for (const auto* data : Datas_) {
+            auto& structJson = dataJson["structs"].emplace_back();
+            structJson["name"] = StringUtil::QualifiedName(*data);
+
+            auto& eventsJson = structJson["events"];
+            eventsJson = inja::json::array();
+            EventsJson_ = &eventsJson;
+            AddEvents(*data);
+        }
+
+        inja::Environment env;
+        env.set_trim_blocks(true);
+        printer << env.render(TEMPLATE, dataJson);
+    }
+
+private:
+    void AddEvents(const clang::RecordDecl& decl) {
     }
 
 private:
     Context& Ctx_;
     const StructDatas& Datas_;
+    inja::json* EventsJson_ = nullptr;
 };
 
 } // namespace
